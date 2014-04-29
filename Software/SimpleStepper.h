9f73874e1d3aad1ffe9293c4b78b1f382dba6af0
Maximiliano Castro <macguionbajo@gmail.com>
2014-04-29 18:00:18 -0400

    Estable, agregado step. Enum para sentido de giro

--- Software/SimpleStepper.h
+++ Software/SimpleStepper.h
@@ -1,13 +1,16 @@
-#include "WProgram.h"
+#include "Arduino.h"
 
 #ifndef SimpleStepper_h
 #define SimpleStepper_h
+typedef enum direccion{
+	CLOCK,
+	CCLOCK
+}Dir;
 class SimpleStepper{
 	public:
 	//métodos y variables públicos
 	SimpleStepper(int pin1, int pin2, int pin3, int pin4);
-	void step(boolean dir);
-	void 
+	void step(Dir dir);
 	private:
 	//métodos y variables privados
 	//posicion en steps, contado desde el inicio del programa
@@ -19,8 +22,9 @@ class SimpleStepper{
 	//pins
 	unsigned char _pins[4];
 	//arreglo de steps
-	unsigned char _steps[8];
+	//unsigned char _steps[8];
 	//indice del step actual
 	int _step_index;
 };
+
 #endif
